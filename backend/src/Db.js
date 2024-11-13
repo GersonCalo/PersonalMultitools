7deda9b522042a5908f0bf4cb60364aaeab1788d
CaloGerson <gersondeba3@gmail.com>
2024-11-13 23:31:39 +0100

    add conection to mongo and create user model

--- backend/src/Db.js
+++ backend/src/Db.js
@@ -1,1 +1,10 @@
-  
+  import mongoose from 'mongoose';
+
+  export const connectDB = async () => {
+    try {
+      await mongoose.connect("mongodb://localhost:27017/PersonalMultitools");
+      console.log('Connected to MongoDB');
+    } catch (error) {
+      console.error('Error connecting to MongoDB:', error.message);
+    }
+  };
--- backend/src/Index.js
+++ backend/src/Index.js
@@ -1,4 +1,6 @@
-import app from './App.js';
+import app from './App.js'
+import {connectDB} from './db.js'
 
+connectDB();
 app.listen(3000)
 console.log('Server running on port 3000')
