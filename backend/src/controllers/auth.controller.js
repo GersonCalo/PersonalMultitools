aec67244b64319e902a8f5610e933d4ab5d5f356
CaloGerson <gersondeba3@gmail.com>
2024-11-14 16:30:49 +0100

    hash the user password

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,14 +1,23 @@
 import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
 
 
 export const register = async (req, res) => {
     const { username, email, password } = req.body
 
     try {
-        const newUser = new User({ username, email, password });
+        const passwordHash = await bcrypt.hash(password, 10);
+
+        const newUser = new User({ username, email, password: passwordHash });
 
         const userSaved =await newUser.save();
-        res.json(userSaved);
+        res.json({
+            id: userSaved._id,
+            username: userSaved.username,
+            email: userSaved.email,
+            createdAt: userSaved.createdAt,
+            updatedAt: userSaved.updatedAt
+        });
     } catch (error) {
         console.log(error);
     }
