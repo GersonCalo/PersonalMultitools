e0de4aba3480fd7aa059d754980c8bf1bad5d1fe
CaloGerson <gersondeba3@gmail.com>
2024-11-14 18:33:58 +0100

    create login logout endpoints

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -27,4 +27,37 @@ export const register = async (req, res) => {
     res.status(500).json({ error: error.message });
   }
 };
-export const login = (req, res) => res.send("login");
+
+export const login = async (req, res) => {
+  const {  email, password } = req.body;
+
+  try {
+
+    const userFound = await User.findOne({ email });
+
+    if (!userFound) return res.status(404).json({ error: "User not found" });
+
+    const isMatch = await bcrypt.compare(password, userFound.password);
+
+    if (!isMatch) return res.status(401).json({ error: "incorrect password" });
+
+    const token = await createAccessToken({ id: userFound._id });
+
+    res.cookie("token", token);
+    res.json({
+        id: userFound._id,
+        username: userFound.username,
+        email: userFound.email,
+        createdAt: userFound.createdAt,
+        updatedAt: userFound.updatedAt,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: error.message });
+  }
+};
+
+export const logout = (req, res) => {
+  res.cookie("token","",{expires: new Date(0)});
+  res.sendStatus(200);
+};
