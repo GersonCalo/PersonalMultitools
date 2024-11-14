f642a8629ca06f9131e945bda020eafec2b659ba
CaloGerson <gersondeba3@gmail.com>
2024-11-14 13:14:24 +0100

    correct the compose file to add MongoExpress, Correct the mongoURI to conect whit data base

--- backend/src/Db.js
+++ backend/src/Db.js
@@ -2,7 +2,7 @@
 
   export const connectDB = async () => {
     try {
-      await mongoose.connect("mongodb://localhost:27017/PersonalMultitools");
+      await mongoose.connect("mongodb://root:example@127.0.0.1:27017/PersonalMultitools?authSource=admin");
       console.log('Connected to MongoDB');
     } catch (error) {
       console.error('Error connecting to MongoDB:', error.message);
