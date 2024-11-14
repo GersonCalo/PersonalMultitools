250e2b07242f77a50c40ba490ff37fdd01bf3d53
CaloGerson <gersondeba3@gmail.com>
2024-11-14 17:42:05 +0100

    create token and return in cooki

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,27 +1,30 @@
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
-
+import { createAccessToken } from "../libs/jwt.js";
 
 export const register = async (req, res) => {
-    const { username, email, password } = req.body
+  const { username, email, password } = req.body;
 
-    try {
-        const passwordHash = await bcrypt.hash(password, 10);
+  try {
+    const passwordHash = await bcrypt.hash(password, 10);
 
-        const newUser = new User({ username, email, password: passwordHash });
+    const newUser = new User({ username, email, password: passwordHash });
 
-        const userSaved =await newUser.save();
-        res.json({
-            id: userSaved._id,
-            username: userSaved.username,
-            email: userSaved.email,
-            createdAt: userSaved.createdAt,
-            updatedAt: userSaved.updatedAt
-        });
-    } catch (error) {
-        console.log(error);
-    }
+    const userSaved = await newUser.save();
 
+    const token = await createAccessToken({ id: userSaved._id });
 
+    res.cookie("token", token);
+    res.json({
+        id: userSaved._id,
+        username: userSaved.username,
+        email: userSaved.email,
+        createdAt: userSaved.createdAt,
+        updatedAt: userSaved.updatedAt
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: error.message });
+  }
 };
-export const login = (req, res) => res.send('login');
+export const login = (req, res) => res.send("login");
